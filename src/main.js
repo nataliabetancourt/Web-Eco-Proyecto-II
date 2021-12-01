f61c94f8f9b59f9ee972b2f478b68a7f05824371
Pola1217 <mariajosepolanco9@gmail.com>
2021-11-30 19:45:14 -0500

    pet cards

--- src/main.js
+++ src/main.js
@@ -3,9 +3,47 @@ import { getDatabase } from 'firebase/database';
 
 import { getFirebaseConfig } from './firebase-config';
 
+import { petCard, pets } from './pet_cards';
+
 // Inicializar firebase
 const firebaseAppConfig = getFirebaseConfig();
 const firebaseApp = initializeApp(firebaseAppConfig);
 
+const pets = document.getElementById('pets');
+const addBtn = document.getElementById('addBtn');
+const addBtn2 = document.getElementById('addBtn2');
 
+function getPets() {
+
+    const db = getDatabase();
+    const dbRef = ref(db, 'Pets/');
+
+    onValue(dbRef, (snapshot) => {
+
+        const data = snapshot.val();
+        actPets(data);
+
+    });
+
+}
+
+function actPets(data) {
+
+    if (data) {
+        
+        pets.innerHTML = " ";
+       
+        Object.keys(data).forEach((key, index)=> {
+            
+            const card = new petCard(data[key])
+
+            pets.appendChild(card.render());
+           
+        });
+
+    }
+
+}
+
+getPets();
 
---
+++ src/pet_cards.js
@@ -0,0 +1,56 @@
+import { getDatabase, ref, onValue, set, update } from 'firebase/database';
+
+export class petCard{
+
+    constructor(pet){
+
+        this.pet = pet;
+
+    }
+
+    render(){
+
+        let card = document.createElement("div");
+        card.className = "cards";
+
+        let petName = document.createElement("h3");
+        petName.className = "petName";
+
+        let age = document.createElement("p");
+        age.className = "age";
+
+        let breed = document.createElement("p");
+        breed.className = "breed";
+
+        let birth = document.createElement("p");
+        birth.className = "birth";
+
+        let color = document.createElement("button");
+        color.className = "color";
+        color.innerHTML = " x ";
+
+        let editBtn = document.createElement("button");
+        editBtn.className = "editBtn";
+        editBtn.innerHTML = "Editar";
+
+        editBtn.addEventListener("click", (e, event) => {
+        
+            const db = getDatabase();
+            let petRef = ref(db, 'Pets/' + this.pet.id);
+
+
+
+        });
+
+
+        card.appendChild(petName);
+        card.appendChild(age);
+        card.appendChild(breed);
+        card.appendChild(birth);
+        card.appendChild(color);
+        card.appendChild(editBtn);
+
+        return card;
+
+    }
+}
